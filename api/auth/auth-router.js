660527a6c07ef2468b79d013dac2058d7f293151
Domenic Scarcella <ls41513@yahoo.com>
2021-04-19 21:32:49 -0400

    Middlewares and some more

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,9 @@
+const router = require('express').Router();
+
+
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
-
+const { checkUsernameFree, checkUsernameExists, checkPasswordLength } = require('../auth/auth-middleware.js');
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -24,7 +27,9 @@
     "message": "Password must be longer than 3 chars"
   }
  */
+router.post('/register', (req, res, next) => {
 
+});
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -41,7 +46,9 @@
     "message": "Invalid credentials"
   }
  */
+router.post('/register', (req, res, next) => {
 
+});
 
 /**
   3 [GET] /api/auth/logout
@@ -58,6 +65,9 @@
     "message": "no session"
   }
  */
+router.get('/register', (req, res, next) => {
 
- 
+});
+
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session= require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const authRouter = require('../api/auth/auth-router.js');
+const usersRouter = require('../api/users/users-router.js');
+
+const server = express();
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -11,15 +17,35 @@ const cors = require("cors");
   Users that do authenticate should have a session persisted on the server,
   and a cookie set on the client. The name of the cookie should be "chocolatechip".
 
-  The session can be persisted in memory (would not be adecuate for production)
+  The session can be persisted in memory (would not be adequate for production)
   or you can use a session store like `connect-session-knex`.
  */
-
-const server = express();
+server.use(session({
+  // config of cookie and session store
+  name: 'chocolatechip',
+  secret: 'keep it secret, keep it safe!', // env not from code
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, // if true the cookie is not set unless it's an https connection
+    httpOnly: false, // if true the cookie is not accessible through document.cookie
+  },
+  resave: false, // some data stores need this set to true
+  saveUninitialized: false, // privacy implications, if false no cookie is set on client unless the req.session is changed
+  store: new KnexSessionStore({
+    knex: require('../data/db-config.js'), // configured instance of knex
+    tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+    sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+    createtable: true, // if the table does not exist, it will create it automatically
+    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+  }),
+}));
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
